80f6e73813b03e60639eb41b02d4220084328e55
João Vitor <joao7791vitor@gmail.com>
2025-01-04 14:24:20 -0300

    feat: HTML sctructure

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path')
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: "",
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: "index-min.js"
+        filename: ""
     }
 }
