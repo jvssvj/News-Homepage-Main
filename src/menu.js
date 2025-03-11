030ea3514c56a9e32eb75f25ec9fafc27e038b8e
João Vitor <joao7791vitor@gmail.com>
2025-03-11 20:33:32 -0300

    fix-menu-mobile

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,18 @@
 const body = document.querySelector('body')
-const menuIcon = document.querySelector('.news__header__nav__menu')
-const menuModal = document.querySelector('.news__header__nav')
-const menuItems = document.querySelector('.news__header__nav__items')
+const menuButton = document.querySelector('.news__header__nav__menu__button')
+const menuModal = document.querySelector('.news__header__nav__items')
+const menuItems = document.querySelectorAll('.news__header__nav__item')
 const overlay = document.querySelector('.overlay')
 
 function toggleMenu() {
-  menuIcon.classList.toggle('open')
+  menuButton.classList.toggle('open')
   menuModal.classList.toggle('open__modal')
-  menuItems.classList.toggle('opacity__items__menu')
+  menuItems.forEach((item) => {
+    item.classList.toggle('opacity__menu__item')
+  })
   overlay.classList.toggle('open__overlay')
+  body.classList.toggle('no__scroll')
 }
 
 overlay.addEventListener('click', toggleMenu)
-menuIcon.addEventListener('click', toggleMenu)
+menuButton.addEventListener('click', toggleMenu)
